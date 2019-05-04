25c83ac6527dda01f666ac177453d4eca8c5ed23
NanduKrish <34903421+NanduKrish@users.noreply.github.com>
2019-05-04 09:27:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var swiper = new Swiper('.product-slider', {
+        speed: 600,
+        spaceBetween: 30,
+        effect: 'cube',        
+        loop: true,
+        navigation: {
+            nextEl: '.next',
+            prevEl: '.prev'
+        },        
+        on: {
+            init: function(){
+                var index = this.activeIndex;
+
+                var target = $('.product-slider__item').eq(index).data('target');
+
+                console.log(target);
+
+                $('.product-img__item').removeClass('active');
+                $('.product-img__item#'+ target).addClass('active');
+            }
+        }
+    });
+
+
+    swiper.on('slideChange', function () {
+        var index = this.activeIndex;
+
+        var target = $('.product-slider__item').eq(index).data('target');
+
+        console.log(target);
+
+        $('.product-img__item').removeClass('active');
+        $('.product-img__item#'+ target).addClass('active');
+
+        if(swiper.isEnd) {
+            $('.prev').removeClass('disabled');
+            $('.next').addClass('disabled');
+        } else {
+            $('.next').removeClass('disabled');
+        }
+
+        if(swiper.isBeginning) {
+            $('.prev').addClass('disabled');
+        } else {
+            $('.prev').removeClass('disabled');
+        }
+    });
+
+    $(".js-fav").on("click", function() {
+        $(this).find('.heart').toggleClass("is-active");
+    });
